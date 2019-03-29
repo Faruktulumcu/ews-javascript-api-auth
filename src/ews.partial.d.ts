d58ec708a26da6c59529d399f56ff716130ac1f2
FarukT <tulumcufaruk@gmail.com>
2019-03-29 11:58:45 +0100

    init

---
+++ src/ews.partial.d.ts
@@ -0,0 +1,28 @@
+import * as Promise from "bluebird";
+import { XMLHttpRequest } from "fetch";
+export interface IXHROptions {
+    type?: string;
+    url: string;
+    user?: string;
+    password?: string;
+    headers?: any;
+    data?: any;
+    responseType?: string;
+    customRequestInitializer?: (request: XMLHttpRequest) => void;
+}
+export interface IXHRProgress {
+    type: 'data' | 'header' | 'end' | 'error';
+    data?: string;
+    chunk?: string;
+    progress?: number;
+    done?: boolean;
+    headers?: any;
+    originalHeaders?: any;
+    error?: any;
+}
+export interface IXHRApi {
+    xhr(xhroptions: IXHROptions, progressDelegate?: (progressData: IXHRProgress) => void): Promise<XMLHttpRequest>;
+    xhrStream(xhroptions: IXHROptions, progressDelegate: (progressData: IXHRProgress) => void): Promise<XMLHttpRequest>;
+    disconnect(): void;
+    apiName?: string;
+}
